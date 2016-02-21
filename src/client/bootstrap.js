05010c562582429364b4907e45b685d328de90f4
Fernando Godinho <cainvampyr@gmail.com>
2016-02-21 19:44:17 +0000

    Refactored the client file structure and eleminated the areas pakage adopting a more 'modular' standerd

--- src/client/bootstrap.js
+++ src/client/bootstrap.js
@@ -3,19 +3,19 @@ var uiRouter    = require('angular-ui-router');
 
 var app         = angular.module('main-app', ['ui.router']);
 
-// Auth Module
-app.factory('session', ['$http', '$rootScope', '$state', '$location', require('./auth/js/service/session')]);
-app.config(['$stateProvider', require('./areas/auth/config')]);
-
 // Core Module
 app.factory('serverAPI', ['$http', require('./core/js/service/serverAPI')]);
 app.directive('layout', require('./core/js/directive/layout'));
 
+// Auth Module
+app.factory('session', ['$http', '$rootScope', '$state', '$location', require('./auth/js/service/session')]);
+app.config(['$stateProvider', require('./auth/config')]);
+
 // Dashboard Module
-app.config(['$stateProvider', require('./areas/dashboard/config')]);
+app.config(['$stateProvider', require('./dashboard/config')]);
 
 // Worlds Module
-app.factory('worldsData', ['serverAPI', require('./areas/worlds/js/service/worldsData')]);
-app.directive('worldsList', ['worldsData', require('./areas/worlds/js/directive/list')]);
-app.config(['$stateProvider', require('./areas/worlds/config')]);
+app.factory('worldsData', ['serverAPI', require('./worlds/js/service/worldsData')]);
+app.directive('worldsList', ['worldsData', require('./worlds/js/directive/list')]);
+app.config(['$stateProvider', require('./worlds/config')]);
 
